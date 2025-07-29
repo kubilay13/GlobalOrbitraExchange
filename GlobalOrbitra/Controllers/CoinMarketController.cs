02c45e16602ac4b253f85724960e5c29a991ff0c
Kubilay <145432835+kubilay13@users.noreply.github.com>
2025-07-29 16:02:46 +0300

    update design

---
+++ GlobalOrbitra/Controllers/CoinMarketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalOrbitra.Controllers
+{
+    public class CoinMarketController : Controller
+    {
+        public IActionResult CoinMarket()
+        {
+            return View();
+        }
+    }
+}
