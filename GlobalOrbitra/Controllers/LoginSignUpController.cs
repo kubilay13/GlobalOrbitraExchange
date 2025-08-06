f6c2fc9d7cc99791cb3f7d941af19ee5cc4ef370
Kubilay <145432835+kubilay13@users.noreply.github.com>
2025-08-06 21:42:47 +0300

    Add Login-Sign Up design

---
+++ GlobalOrbitra/Controllers/LoginSignUpController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalOrbitra.Controllers
+{
+    public class LoginSignUpController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
