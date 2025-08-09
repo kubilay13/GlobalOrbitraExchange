6bc74aac0cd4998754afc774a28155a09b8ed07e
Kubilay <145432835+kubilay13@users.noreply.github.com>
2025-08-09 13:42:56 +0300

    Add widthdraw design

---
+++ GlobalOrbitra/Controllers/WidthdrawDepositController.cs
@@ -0,0 +1,20 @@
+﻿using GlobalOrbitra.Models.UserModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalOrbitra.Controllers
+{
+    public class WidthdrawDepositController : Controller
+    {
+        public IActionResult Widthdraw()
+        {
+            var assets = new List<Asset>
+        {
+            new Asset { Symbol = "BTC", Name = "Bitcoin", ImageUrl = "/images/btc.png" },
+            new Asset { Symbol = "ETH", Name = "Ethereum", ImageUrl = "/images/eth.png" },
+            new Asset { Symbol = "USDT", Name = "Tether", ImageUrl = "/images/usdt.png" }
+        };
+
+            return View(assets);
+        }
+    }
+}
